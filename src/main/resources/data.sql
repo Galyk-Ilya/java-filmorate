69d8b212278d87e7efeac43ed316044b10eabff9
Ilya <xcaz@inbox.ru>
2023-05-02 23:28:43 +0400

    11.1 code style #4 added tests #1

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,9 +4,9 @@ MERGE INTO MPA (id,name) VALUES (3,'PG-13');
 MERGE INTO MPA (id,name) VALUES (4,'R');
 MERGE INTO MPA (id,name) VALUES (5,'NC-17');
 
-MERGE INTO GENRE (id,name) VALUES (1,'Комедия');
-MERGE INTO GENRE (id,name) VALUES (2,'Драма');
-MERGE INTO GENRE (id,name) VALUES (3,'Мультфильм');
-MERGE INTO GENRE (id,name) VALUES (4,'Триллер');
-MERGE INTO GENRE (id,name) VALUES (5,'Документальный');
-MERGE INTO GENRE (id,name) VALUES (6,'Боевик');
+MERGE INTO GENRE (id,name) VALUES (1,'Comedy');
+MERGE INTO GENRE (id,name) VALUES (2,'Drama');
+MERGE INTO GENRE (id,name) VALUES (3,'Cartoon');
+MERGE INTO GENRE (id,name) VALUES (4,'Thriller');
+MERGE INTO GENRE (id,name) VALUES (5,'Documentary');
+MERGE INTO GENRE (id,name) VALUES (6,'Action');
